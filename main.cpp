b5e024111b0da5e8633397e7593710ec4bc6355a
skovoroad <skovoroad@gmail.com>
2021-06-21 16:21:01 +0300

    now it works

--- main.cpp
+++ main.cpp
@@ -12,17 +12,21 @@ namespace core {
   struct session_event {
     std::string text;
 
-    bool need_response = false;
+//    bool need_response = false;
     size_t sender_id = 0;
   };
 
   struct messenger {
     virtual void send(size_t dialog_id_to, session_event event)  = 0;
-    virtual void request(size_t dialog_id_from, size_t dialog_id_to, session_event event)  = 0;
+    virtual void request( size_t dialog_id_to, 
+                          session_event request,
+                          session_event& response
+                        )  = 0;
   };
 
   struct dialog {
       virtual void handle (session_event ) = 0;
+      virtual void response (session_event, session_event& ) = 0;
       virtual size_t id()  = 0;
   };
 }
@@ -38,50 +42,34 @@ namespace project {
     dialog_impl(core::messenger& m, size_t i) : messenger_(m), id_(i) {}
 
     void handle (core::session_event e ) {
-      std::cout << std::this_thread::get_id() <<  " dialog " << id_ << " received " << e.text << std::endl;
+      std::cout << std::this_thread::get_id() <<  " dialog_impl: dialog " << id_ << " received " << e.text << std::endl;
+ 
+      if(!(++received_ % 10)) {
+        std::stringstream ostr;
+        size_t receiver =  id_ / 2;
+        if(receiver == id_)
+          return; // just for simplicity
+        ostr << " send request from " << id_ << " to " << receiver;
+        std::cout << ostr.str() << std::endl;
 
-      if(expected_response_from_) {
-        if(e.sender_id != expected_response_from_ ) {
-          std::cout << "============ DISORDER " << id_ << " ============  expected response from "
-            << expected_response_from_
-            << " received message from "
-            << e.sender_id << " \"" << e.text << "\""
-            << std::endl;
-          }
-          else { 
-            expected_response_from_ = 0; 
-            std::cout << "============ ORDER " << id_ << " ============  expected response from "
-              << expected_response_from_
-              << " received message from "
-              << e.sender_id << " \"" << e.text << "\""
+        core::session_event resp;
+        messenger_.request(receiver, core::session_event{ ostr.str(), id_}, resp ); 
+
+        std::cout << std::this_thread::get_id() << " dialog_impl: received response from " << e.sender_id 
+              << " to " << id_
+              << " \"" << e.text << "\""
               << std::endl;
-          }
+ 
       }
-
-      if(e.need_response) 
-        send_response(e);
-      if(!(++received_ % 10))
-        send_request();
     }
 
-    void send_response(core::session_event& e) {
+    void response (core::session_event e, core::session_event& r) {
       std::stringstream ostr;
       ostr << "response to \"" << e.text << "\" from " << id_ << " to " << e.sender_id;
-      std::cout << ostr.str() << std::endl;
-      messenger_.send(e.sender_id, core::session_event{ ostr.str(), false, id_} ); 
-    }
+      std::cout << std::this_thread::get_id() << " dialog_impl: response: " << ostr.str() << std::endl;
 
-    void send_request() {
-      std::stringstream ostr;
-      size_t receiver =  id_ / 2;
-      if(receiver == id_)
-        return; // just for simplicity
-      ostr << "send request from " << id_ << " to " << receiver;
-      std::cout << ostr.str() << std::endl;
-      expected_response_from_ = receiver;
-      messenger_.send(receiver, core::session_event{ ostr.str(), true, id_} ); 
+      r = core::session_event{ ostr.str(), id_} ; 
     }
-
     size_t id() { return id_; }
   };
 
@@ -91,43 +79,6 @@ namespace project {
 }
 
 namespace core {
-
-  struct dialog_executor {
-    std::shared_ptr<dialog> dialog_;
-    boost::fibers::buffered_channel< session_event > channel_;
-    session_event current_;
-    boost::fibers::fiber fiber_;
-    std::atomic<bool> stop_ = false;
- 
-    dialog_executor( dialog *d, size_t queue_size)
-      : dialog_(d),
-        channel_ (queue_size),
-        fiber_ ( [this]{ read_loop(); }) 
-        {}
-
-    ~dialog_executor() {
-       if(fiber_.joinable())
-         fiber_.join();
-    }
-
-    void read_loop() {
-      using namespace std::chrono_literals;
-      while ( boost::fibers::channel_op_status::success == channel_.pop_wait_for(current_, 1s) ) {
-        if(stop_)
-          break;
-        dialog_->handle(current_);
-      }
-    }
-
-    void send(session_event event) {
-      channel_.try_push(event);
-    }
-
-    void request(size_t dialog_id_from, session_event event) {
-      channel_.try_push(event);
-    }
-  };
-
   struct fiber_threads {
      bool finished = false;
      std::mutex mtx{};
@@ -137,6 +88,8 @@ namespace core {
 
      void run(size_t threads_count) {
        boost::fibers::use_scheduling_algorithm< boost::fibers::algo::shared_work >();
+       if(!threads_count)
+         return;
        while(threads_count --)
          threads_.emplace_back (
            [&]() {
@@ -160,9 +113,82 @@ namespace core {
      } 
   };
 
-  struct engine : public messenger {
-    const size_t dialog_count = 1000;
-    const size_t queue_size = 2;
+  struct session_event_ctx {
+    session_event_ctx(const session_event& e) : event(e) {} 
+
+    session_event event;
+    session_event * response = nullptr;
+
+    boost::fibers::condition_variable_any cond{};
+    std::mutex mtx{};
+    bool responsed = false;
+ };
+ 
+  struct dialog_executor {
+    std::shared_ptr<dialog> dialog_;
+    boost::fibers::buffered_channel< std::shared_ptr<session_event_ctx> > channel_;
+    std::shared_ptr<session_event_ctx> current_;
+    boost::fibers::fiber fiber_;
+    std::atomic<bool> stop_ = false;
+ 
+    dialog_executor( dialog *d, size_t queue_size)
+      : dialog_(d),
+        channel_ (queue_size),
+        fiber_ ( [this]{ read_loop(); }) 
+        {}
+
+    ~dialog_executor() {
+       if(fiber_.joinable())
+         fiber_.join();
+    }
+
+    void read_loop() {
+      using namespace std::chrono_literals;
+      while ( boost::fibers::channel_op_status::success == channel_.pop_wait_for(current_, 1s) ) {
+        if(stop_)
+          break;
+        if(current_->response) {
+          std::cout << std::this_thread::get_id() << " dialog_executor: received request: " << current_->event.text << std::endl;
+ 
+          dialog_->response(current_->event, *current_->response);
+          {
+            std::unique_lock<std::mutex> lk(current_->mtx);
+            current_->responsed = true;
+            std::cout << std::this_thread::get_id() << " dialog_executor: responded to request: " << current_->event.text << std::endl;
+            current_->cond.notify_all();
+         }
+        }
+        else {
+//          std::cout << "dialog_executor: received message: " << current_->event->text << std::endl;
+          dialog_->handle(current_->event);
+        }
+      }
+    }
+
+    void send(session_event event) {
+      auto ctx = std::make_shared<session_event_ctx>( event );  
+      channel_.push( ctx ); // TODO: what if not?
+    }
+
+
+    void request( session_event request,
+                  session_event& response) 
+    { // callee fiber
+      auto ctx = std::make_shared<session_event_ctx>( request ); 
+      ctx->response = &response;
+      std::cout << std::this_thread::get_id() << " dialog_executor: sending request: " << ctx->event.text << std::endl;
+ 
+      std::unique_lock<std::mutex> lk( ctx->mtx);
+      channel_.push( ctx ); // TODO: what if not?
+      std::cout << std::this_thread::get_id() << " dialog_executor: waiting response: " << ctx->event.text << std::endl;
+      ctx->cond.wait( lk, [&]() { return ctx->responsed;} );
+    }
+
+ };
+
+ struct engine : public messenger {
+    const size_t dialog_count = 5;
+    const size_t queue_size = 8;
     const size_t messages_count = 1000000000;
     const size_t threads_count = 0;
     fiber_threads ft;
@@ -172,14 +198,19 @@ namespace core {
       dialogs[dialog_id_to]->send(event);
     }
 
-    void request(size_t dialog_id_from, size_t dialog_id_to, session_event event) {
+/*    void request(size_t dialog_id_from, size_t dialog_id_to, session_event event) {
       dialogs[dialog_id_to]->request(dialog_id_from, event);
+    }*/
+    void request( size_t dialog_id_to, 
+                  session_event request,
+                  session_event& response) 
+    {
+      dialogs[dialog_id_to]->request(request, response); 
     }
  
     void run() {
       try {
-        if(threads_count)
-          ft.run(threads_count);
+        ft.run(threads_count);
 
         for(size_t i = 0; i < dialog_count; ++i) {
           std::shared_ptr<core::dialog_executor> d(new core::dialog_executor {
@@ -192,14 +223,13 @@ namespace core {
         for(size_t i = 0; i < messages_count; ++i) {
           size_t dialog_index = i % dialog_count;
           core::session_event event{ std::to_string(i) };
-          dialogs[dialog_index]->channel_.push(event);
+          dialogs[dialog_index]->send(event);
         }
 
         for(size_t i = 0; i < dialog_count; ++i) {
           dialogs[i]->stop_ = true;
         }
-        if(threads_count)
-          ft.stop();
+        ft.stop();
       }
       catch(std::exception& e) {
         std::cerr << "caught: " << e.what() << std::endl;
